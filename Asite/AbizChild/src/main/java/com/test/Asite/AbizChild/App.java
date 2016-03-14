fca873847783df12479351c91dcea87d9a35c535
sese5858 <sese5858@naver.com>
2016-03-14 10:06:30 +0900

    test commit

---
+++ Asite/AbizChild/src/main/java/com/test/Asite/AbizChild/App.java
@@ -0,0 +1,13 @@
+package com.test.Asite.AbizChild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Bsite/BbizParent3/src/main/java/com/test/Bsite/BbizParent3/App.java
@@ -0,0 +1,13 @@
+package com.test.Bsite.BbizParent3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
